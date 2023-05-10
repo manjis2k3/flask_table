1072f2aefd3a6c9bb57611170d3b42c795d2242c
manjis2k3 <37095230+manjis2k3@users.noreply.github.com>
2023-05-10 22:55:15 +0100

    Update server_table.py

--- server_table.py
+++ server_table.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-da2-xpfhwqjdobaaxkdcf27qfvtiou
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
